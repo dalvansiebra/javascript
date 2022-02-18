b5b4d4bd82fa509bc80ed4de3272f48106c4dfd2
Dalvan Siebra <dalvansiebra@outlook.com>
2022-02-18 00:37:31 -0300

    Update

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,27 @@
-alert('Olá!')
+var intervald = window.setInterval(() => {carregar()
+        
+},1000);
+
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    var segundo = data.getSeconds()
+    msg.innerHTML = `Agora são ${hora}:${minuto}:${segundo}`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#FC7C6F'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#fdd3a3'
+    } else {
+        //BOA NOITE
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#012C3D'
+    }
+}
+
