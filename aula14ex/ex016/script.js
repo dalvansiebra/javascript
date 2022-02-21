73f2f0687b23b52f8d970b4ebe265790f01a33c5
Dalvan Siebra <dalvansiebra@outlook.com>
2022-02-21 20:44:26 -0300

    ambiente.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,15 +5,30 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO: 1.')
+            p = 1
         }
+        
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += ` Fim! \u{1F3c1}`
     }
 }
